f7f0f6a92cde6de88e93c58a4a8c2e0000737339
baskrahmer <baskrahmer@gmail.com>
2024-03-28 13:46:59 -0400

    Clean up data processing

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ from train import train
 if __name__ == "__main__":
     c = Config()
 
-    data_path = filter_data(c)
-    model = train(c, data_path=data_path, model=None, mlm=True) if c.mlm_train_steps else None
-    model = train(c, data_path=data_path, model=model) if c.train_steps else None
+    if c.mlm_train_steps:
+        data_path = filter_data(c, mlm=True)
+        model = train(c, data_path=data_path, model=None, mlm=True) if c.mlm_train_steps else None
+    else:
+        model = None
+    model = train(c, data_path=filter_data(c), model=model) if c.train_steps else None
--- src/preprocess.py
+++ src/preprocess.py
@@ -7,54 +7,46 @@ from transformers import PreTrainedTokenizerBase
 from config import Config
 
 
-def filter_data(c: Config):
-    # TODO separate settings for MLM dataset
-    config_hash = str(hash((_ for _ in [c.sample_size])))
+def filter_data(c: Config, mlm: bool = False) -> str:
+    config_hash = str(hash((_ for _ in [c.sample_size, c.test_size])))
 
     products_filename = "products.jsonl"
-    filtered_products_filename = "products_filtered.jsonl"
-
+    filtered_products_filename = "products_filtered.jsonl" if not mlm else "products_mlm.jsonl"
     data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
     ciqual_path = os.path.join(data_dir, "ciqual.csv")
     products_path = os.path.join(data_dir, products_filename)
     output_path = os.path.join(data_dir, config_hash)
+
     if not os.path.exists(output_path):
         os.makedirs(output_path, exist_ok=True)
     filtered_products_path = os.path.join(output_path, filtered_products_filename)
+
+    # Return cached data path if it exists
     if c.cache_data and os.path.exists(filtered_products_path):
         return filtered_products_path
 
     ciqual_data = pd.read_csv(ciqual_path)
-    ciqual_to_agb = {
-        str(c): str(a) for c, a in zip(ciqual_data["Code CIQUAL"], ciqual_data["Code AGB"])
-    }
+    ciqual_to_agb = {str(c): str(a) for c, a in zip(ciqual_data["Code CIQUAL"], ciqual_data["Code AGB"])}
     agb_set = set(ciqual_data["Code AGB"])
     del ciqual_data
 
-    columns = [
-        "product_name",
-        "lang",
-        # "ecoscore_grade",
-        # "ecoscore_tags",
-        # "ecoscore_data",
-        # "ciqual_food_name_tags",
-        # "categories_old",
-        # "category_properties",
-        "categories_properties"
-    ]
+    columns = ["product_name", "lang"]
+    # Note: other potential columns of interest include ecoscore_grade, ecoscore_tags, ecoscore_data,
+    #  ciqual_food_name_tags, categories_old,  category_properties, categories_properties and categories_properties
 
     i = 0
-    language_counts = {}
-    labelled_language_counts = {}
-    labelled_class_counts = {}
+    lang_frequencies, label_frequencies = {}, {}
     with open(products_path) as f, open(filtered_products_path, 'w') as out_file:
         for line in tqdm(f):
-            point = json.loads(line)
+            product = json.loads(line)
+            if mlm:
+                filtered_entry = {c: product.get(c) for c in columns}
+                filtered_entry["label"] = label
+                out_file.write(json.dumps(filtered_entry))
+                out_file.write('\n')
 
-            language_counts[point.get("lang")] = language_counts.get(point.get("lang"), 0) + 1
-
-            if "categories_properties" in point:
-                categories = point.pop("categories_properties")
+            elif "categories_properties" in product:
+                categories = product.pop("categories_properties")
                 if categories.get("agribalyse_food_code:en") in agb_set:
                     label = categories.get("agribalyse_food_code:en")
                 elif categories.get("ciqual_food_code:en") in ciqual_to_agb:
@@ -65,16 +57,21 @@ def filter_data(c: Config):
                 else:
                     continue
 
-                labelled_language_counts[point.get("lang")] = labelled_language_counts.get(point.get("lang"), 0) + 1
-                labelled_class_counts[label] = labelled_class_counts.get(label, 0) + 1
-
-                filtered_entry = {c: point.get(c) for c in columns}
+                filtered_entry = {c: product.get(c) for c in columns}
                 filtered_entry["label"] = label
                 out_file.write(json.dumps(filtered_entry))
                 out_file.write('\n')
-                i += 1
-                if c.sample_size and i > c.sample_size:
-                    break
+
+            else:
+                continue
+
+            lang = product.get("lang")
+            lang_frequencies[lang] = lang_frequencies.get(lang, 0) + 1
+            label_frequencies[label] = label_frequencies.get(label, 0) + 1
+
+            i += 1
+            if c.sample_size and i > c.sample_size:
+                break
 
     if c.data_analysis:
         # TODO make histogram
@@ -86,7 +83,7 @@ def filter_data(c: Config):
 def prepare_inputs(sample: dict, tokenizer: PreTrainedTokenizerBase, tokenizer_kwargs: dict,
                    class_to_idx: dict, class_to_co2e: dict) -> dict:
     # TODO plural keys are a bit misleading here - singular is simplified and therefore preferred
-    sample["encodings"] = tokenizer(sample.pop('product_name'), **tokenizer_kwargs)
+    sample['encodings'] = tokenizer(sample.pop('product_name'), **tokenizer_kwargs)
     sample['regressands'] = class_to_co2e[sample['label']]
     sample['classes'] = class_to_idx[sample['label']]
     return sample
