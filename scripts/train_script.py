80c10fdeaac70d106234862f92ec56ebacf0e898
baskrahmer <baskrahmer@gmail.com>
2024-04-20 11:40:12 +0200

    Update model_name

--- scripts/train_script.py
+++ scripts/train_script.py
@@ -4,7 +4,7 @@ from src.main import main
 c = Config(
     use_wandb=True,
     sample_size=10000,
-    model_name="distilbert/distilbert-base-multilingual-cased",
+    model_name="sentence-transformers/distiluse-base-multilingual-cased-v2",
     train_steps=1000,
     val_steps=100,
 )
